66ca72f1a92bfac46e4598d1e65f38f8d221ea64
bluefacebaby51k-cpu <bluefacebaby51k@gmail.com>
2026-05-09 10:00:32 +1000

    Update apps.js

--- apps.js
+++ apps.js
@@ -2,7 +2,7 @@ let entries = [];
 let pool = 0;
 
 // JOIN FUNCTION
-https://script.google.com/macros/s/AKfycbxFDCUUI9Qj1-9Mr5t3kDD0LcGcnUubl1rvNjNkHyzoe_-5ziAG4mUElY_3iNQJOMVffw/exec;
+https://script.google.com/macros/s/AKfycbxFDCUUI9Qj1-9Mr5t3kDD0LcGcnUubl1rvNjNkHyzoe_-5ziAG4mUElY_3iNQJOMVffw/exec
 
 function joinDraw() {
   const name = document.getElementById("name").value;
